b57de06bf320f89ac6dfea8d4327e0e2334ed341
Bruno Fillion <Bruno.Fillion@ia.ca>
2021-02-15 15:05:31 -0500

    Globalization

--- src/ksql.machina/ksql.machina/Program.cs
+++ src/ksql.machina/ksql.machina/Program.cs
@@ -20,7 +20,9 @@ namespace ksql.machina
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    .UseUrls("http://*:8080")
+                    .UseStartup<Startup>();
                 });
     }
 }
